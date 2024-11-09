9afa855c30b21b3dab5b73e723b4c737786b9caa
yassine20011 <medalgamer47@gmaim.com>
2024-11-09 02:20:53 +0100

    Update application title and change dashboard route to home

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
