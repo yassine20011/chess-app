a659539c763a9600dfc29f4c1eb1e0068e72e99a
yassine20011 <medalgamer47@gmaim.com>
2024-11-09 02:22:52 +0100

    Implement broadcasting for MoveMade event and simplify constructor

--- app/Events/MoveMade.php
+++ app/Events/MoveMade.php
@@ -7,40 +7,22 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-
-class MoveMade
+class MoveMade implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     protected $game;
-    protected $move;
-    protected $player_id;
 
-    public function __construct($game, $move, $player_id)
+    public function __construct($game)
     {
         $this->game = $game;
-        $this->move = $move;
-        $this->player_id = $player_id;
     }
 
     public function broadcastOn()
     {
         return new PrivateChannel('game.' . $this->game->id);
-    }
-
-    public function broadcastWith()
-    {
-        return [
-            'move' => [
-                'game_id' => $this->game->id,
-                'player_id' => $this->player_id,
-                'from' => $this->move['from'],
-                'to' => $this->move['to'],
-                'piece' => $this->move['piece'],
-                'fen' => $this->move['fen'],
-            ]
-        ];
     }
 
 }
